0c9d9be1aa3fc29de893b2126561fbaddf647e10
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-08-21 14:07:43 -0300

    Implementa package_validation no ProjectSerializer.

--- api/package_validation.py
+++ api/package_validation.py
@@ -5,7 +5,7 @@ def package_versions(package_name):
     url = "https://pypi.org/pypi/%s/json" % (package_name,)
     data = requests.get(url)
     if data.status_code == 404:
-        return "One or more packages don't exist"
+        return "error"
 
     versions = data.json()["releases"].keys()
     return versions
--- api/serializers.py
+++ api/serializers.py
@@ -1,19 +1,20 @@
 from rest_framework import serializers
 
 from .models import PackageRelease, Project
+from api.package_validation import package_versions
 
 
 class PackageSerializer(serializers.ModelSerializer):
     class Meta:
         model = PackageRelease
-        fields = ['name', 'version']
-        extra_kwargs = {'version': {'required': False}}
+        fields = ["name", "version"]
+        extra_kwargs = {"version": {"required": False}}
 
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ['name', 'packages']
+        fields = ["name", "packages"]
 
     packages = PackageSerializer(many=True)
 
@@ -21,5 +22,17 @@ class ProjectSerializer(serializers.ModelSerializer):
         # TODO
         # - Processar os pacotes recebidos
         # - Persistir informações no banco
-        packages = validated_data['packages']
-        return Project(name=validated_data['name'])
+        packages = validated_data["packages"]
+        for package in packages:
+            versions = package_versions(package["name"])
+            if versions == "error":
+                return {"error": "One or more packages don't exist"}
+            if (
+                "version" in package.keys()
+                and package["version"] not in versions
+            ):
+                return {"error": "One or more packages don't exist"}
+            if "version" not in package.keys():
+                package["version"] = list(versions)[-1]
+
+        return Project(name=validated_data["name"])
